6f213419eb190a25cb68088c748d906b28050e78
Daria <fordariasek@gmail.com>
2020-07-24 16:55:02 +0300

    Updates

---
+++ precache-manifest.2f7a81a81f40ff3854d0348ddc26d448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb19545ac842de8ef1bf0e32cae20f5",
+    "url": "/react-yalantis-internship/index.html"
+  },
+  {
+    "revision": "2729c677156917420e4c",
+    "url": "/react-yalantis-internship/static/css/main.1b305cd4.chunk.css"
+  },
+  {
+    "revision": "0f1b754a6827349894e9",
+    "url": "/react-yalantis-internship/static/js/2.7d43edab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-yalantis-internship/static/js/2.7d43edab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2729c677156917420e4c",
+    "url": "/react-yalantis-internship/static/js/main.53954f06.chunk.js"
+  },
+  {
+    "revision": "fdb3cdde63b3b46a0d46",
+    "url": "/react-yalantis-internship/static/js/runtime-main.845473f8.js"
+  }
+]);
